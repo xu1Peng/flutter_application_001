726cb64342f0fb013bd49d4fc9e156f027936599
xuPeng <c18609236670@163.com>
2024-11-13 19:06:44 +0800

    测试提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(myApp(
+
+  ));
 } 
 
 class myApp extends StatelessWidget{
@@ -14,9 +16,15 @@ class myApp extends StatelessWidget{
         appBar: AppBar(
           title: Text('测试demo'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('hello, worldajdhakdha'),
+                  // Text('hello, worldajdhakdha'),
+                  // Text('hello, worldajdhakdha'),
         ),
+
+        child: new Image.network('https://img-blog.csdnimg.cn/20201014180756925.png?x-oss-process=image/resize,m_fixed,h_64,w_64'),
+
+
       ),
     );
   }
