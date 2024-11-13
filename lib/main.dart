884dbaa34fc295932a95498c14bdacb46a2b8541
xuPeng <c18609236670@163.com>
2024-11-13 19:59:31 +0800

    Revert "测试提交"
    This reverts commit 726cb64342f0fb013bd49d4fc9e156f027936599.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp(
-
-  ));
+  runApp(myApp());
 } 
 
 class myApp extends StatelessWidget{
@@ -16,15 +14,9 @@ class myApp extends StatelessWidget{
         appBar: AppBar(
           title: Text('测试demo'),
         ),
-        body: const Center(
+        body: Center(
           child: Text('hello, worldajdhakdha'),
-                  // Text('hello, worldajdhakdha'),
-                  // Text('hello, worldajdhakdha'),
         ),
-
-        child: new Image.network('https://img-blog.csdnimg.cn/20201014180756925.png?x-oss-process=image/resize,m_fixed,h_64,w_64'),
-
-
       ),
     );
   }
